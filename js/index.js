d5cf72a04dba9276f28e1d8815dfac2f5939e28a
ElDiGio <samuel.dgv@gmail.com>
2023-02-07 12:52:38 +0100

    getData from json-server

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const getData = async () => {
+  const response = await (await fetch('http://localhost:3000/people')).json();
+
+  Array.from(response).forEach(value => {
+    console.log(value);
+  });
+};
+
+getData();
